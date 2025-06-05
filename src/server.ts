5be48ee9360d8b9e63dc8fb28e86a4fe0dbdc1fa
André <andredev.fe@gmail.com>
2025-06-05 16:00:15 -0300

    fix: set server to listen on 0.0.0.0 for Render

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: '0.0.0.0', // ⬅️ ISSO É ESSENCIAL!
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    console.log(`HTTP Server Running on port ${env.PORT}`)
   })
